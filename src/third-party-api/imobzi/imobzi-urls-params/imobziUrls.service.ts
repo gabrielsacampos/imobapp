44af27c8db48e8ab4ced3463c4a7ecddd5a6cc6e
Gabriel <gabrielsa95.2@gmail.com>
2023-09-22 10:37:29 -0300

    fixing params from functions bigint to Number

--- src/third-party-api/imobzi/imobzi-urls-params/imobziUrls.service.ts
+++ src/third-party-api/imobzi/imobzi-urls-params/imobziUrls.service.ts
@@ -3,7 +3,7 @@ export class ImobziUrlService {
     return `https://api.imobzi.app/v1/invoices?page=${page}&status=paid&start_at=2000-01-01&end_at=2100-01-01&contract_type=all&order_by=desc`;
   }
 
-  urlInvoiceDetail(invoiceId: bigint): string {
+  urlInvoiceDetail(invoiceId: number): string {
     return `https://api.imobzi.app/v1/invoice/${invoiceId}`;
   }
 
@@ -11,7 +11,7 @@ export class ImobziUrlService {
     return `https://api.imobzi.app/v1/properties?smart_list=${status}&cursor=${cursor}`;
   }
 
-  urlPropertyDetails(propertyId: bigint): string {
+  urlPropertyDetails(propertyId: number): string {
     return `https://api.imobzi.app/v1/property/${propertyId}`;
   }
 
@@ -19,11 +19,11 @@ export class ImobziUrlService {
     return `https://api.imobzi.app/v1/contacts?cursor=${cursor}`;
   }
 
-  urlPersonDetails(personId: bigint): string {
+  urlPersonDetails(personId: number): string {
     return `https://api.imobzi.app/v1/person/${personId}`;
   }
 
-  urlOrganizationDetails(organizationId: bigint): string {
+  urlOrganizationDetails(organizationId: number): string {
     return `https://api.imobzi.app/v1/organization/${organizationId}`;
   }
 }
