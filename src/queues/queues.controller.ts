ad8e42519713f948335701d41247a16998d61ac9
Gabriel <gabrielsa95.2@gmail.com>
2023-10-28 09:30:07 -0300

    fix(QueueController): QueueController calls QueueService

--- src/queues/queues.controller.ts
+++ src/queues/queues.controller.ts
@@ -1,27 +1,27 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { QueueGranatumService } from './queue-granatum/queue-granatum.service';
 import { FetchDb } from './queue-granatum/interfaces/imobziQueue.interface';
 import { QueueImobziService } from './queue-imobzi/queue-imobzi.service';
 import { StoreDb } from './queue-imobzi/interfaces/imobziQueue.interface';
 import { QueuesService } from './queues.service';
+import { QueueGranatumService } from './queue-granatum/queue-granatum.service';
 
 @Controller('queues')
 export class QueuesController {
   constructor(
     private readonly queuesService: QueuesService,
     private readonly imobziQueueService: QueueImobziService,
-    private readonly QueueGranatumService: QueueGranatumService,
+    private readonly queueGranatumService: QueueGranatumService,
   ) {}
 
   @Post('/imobzi/update-db')
   async syncImobziDb(@Body() data: StoreDb) {
-    this.imobziQueueService.storeDb(data);
+    this.queuesService.imobziStoreDb(data);
     return { message: 'ImobziQueue updating database' };
   }
 
   @Post('/granatum/sync')
   async syncGranatumDb(@Body() data: FetchDb) {
-    this.QueueGranatumService.fetchDb(data);
+    this.queuesService.granatumSyncDb(data);
     return { message: 'GranatumQueue fetching database' };
   }
 }
