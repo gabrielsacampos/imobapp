d46e646e6930c6dce03b36785c552d4eb72fa04b
Gabriel <gabrielsa95.2@gmail.com>
2023-10-30 13:00:38 -0300

    removing webhooks models

--- src/repository/webhooks/dtos/update-webhook.dtos.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateWebhookDTO } from './create-webhook.dtos';
-
-export class UpdateWebhookDTO extends PartialType(CreateWebhookDTO) {}
--- src/repository/webhooks/entities/webhook.entity.ts
+++
@@ -1,6 +0,0 @@
-export class Webhook {
-  id?: number;
-  event: string;
-  done: boolean;
-  id_entity_imobzi: string;
-}
