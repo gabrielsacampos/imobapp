bf88886fcbc6848bd37beed77b34d4209d80d4d5
Gabriel <gabrielsa95.2@gmail.com>
2023-10-30 15:08:49 -0300

    fix(buildingsService) unity-tests completed

--- src/repository/buildings/buildings.service.ts
+++ src/repository/buildings/buildings.service.ts
@@ -9,71 +9,88 @@ export class BuildingsService {
 
   // it can create properties from imobzi or not
   async create(data: CreateBuildingDTO) {
-    const existsBuildingId = await this.prisma.building.findFirst({
-      where: { id_imobzi: data.id_imobzi },
-    });
+    try {
+      const existsBuildingId = await this.prisma.building.findFirst({
+        where: { id_imobzi: data.id_imobzi },
+      });
 
-    if (existsBuildingId) {
-      throw new NotAcceptableException(`ID ${data.id_imobzi} already exists at building: ${data.name}`);
+      if (existsBuildingId) {
+        throw new NotAcceptableException(`ID ${data.id_imobzi} already exists at building: ${data.name}`);
+      }
+
+      const existsBuildingName = await this.prisma.building.findFirst({
+        where: { name: data.name },
+      });
+
+      if (existsBuildingName) {
+        throw new NotAcceptableException(`Building name ${data.name} already exists`);
+      }
+
+      return await this.prisma.building.create({ data });
+    } catch (error) {
+      throw new Error(error);
     }
-
-    const existsBuildingName = await this.prisma.building.findFirst({
-      where: { name: data.name },
-    });
-
-    if (existsBuildingName) {
-      throw new NotAcceptableException(`Building name ${data.name} already exists`);
-    }
-
-    await this.prisma.building.create({ data });
-
-    return { message: `${data.name} building created` };
   }
 
   async findAll() {
-    const all = await this.prisma.building.findMany();
-    return all.map((element) => {
-      const id = element.id.toString();
-      delete element.id;
-      return { ...element, id };
-    });
+    try {
+      const all = await this.prisma.building.findMany();
+      return all.map((element) => {
+        const id = element.id.toString();
+        delete element.id;
+        return { ...element, id };
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 
   async findById(id: string) {
-    const found = await this.prisma.building.findUnique({
-      where: { id: Number(id) },
-    });
+    try {
+      const found = await this.prisma.building.findUnique({
+        where: { id: Number(id) },
+      });
 
-    if (!found) {
-      throw new NotAcceptableException(`ID: ${id} not found at buildings`);
+      if (!found) {
+        throw new NotAcceptableException(`ID: ${id} not found at buildings`);
+      }
+      return { found };
+    } catch (error) {
+      throw new Error(error);
     }
-    return { found };
   }
 
   // update using db id.
   async update(id: string, data: UpdateBuildingDTO) {
-    const buildingExists = await this.prisma.building.findMany({
-      where: { id: Number(id) },
-    });
+    try {
+      const buildingExists = await this.prisma.building.findFirst({
+        where: { id: Number(id) },
+      });
 
-    if (!buildingExists) {
-      throw new NotAcceptableException(`ID: ${id} does not exists at buildings`);
+      if (!buildingExists) {
+        throw new NotAcceptableException(`ID: ${id} does not exists at buildings`);
+      }
+
+      return await this.prisma.building.update({
+        where: {
+          id: Number(id),
+        },
+        data,
+      });
+    } catch (error) {
+      throw new Error(error);
     }
-
-    const updated = await this.prisma.building.update({
-      where: {
-        id: Number(id),
-      },
-      data,
-    });
-    return updated;
   }
 
   async upsert(data: CreateBuildingDTO) {
-    await this.prisma.building.upsert({
-      where: { id_imobzi: data.id_imobzi },
-      update: data,
-      create: data,
-    });
+    try {
+      await this.prisma.building.upsert({
+        where: { id_imobzi: data.id_imobzi },
+        update: data,
+        create: data,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }
