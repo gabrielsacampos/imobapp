4bf4a4514bda2e5cd94446498dea4441d8c9100e
Gabriel <gabrielsa95.2@gmail.com>
2023-11-07 15:05:07 -0300

    fix(OrgService)unity test

--- src/repository/organizations/organizations.service.spec.ts
+++ src/repository/organizations/organizations.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { InMemoryOrganizationsRepository } from '../../../test/server-repositories/inMemoryOrganizationsRepository/inMemoryOrganizationsRepository';
+import { inMemoryOrganizationsRepositoryMock } from '../../../test/server-repositories/inMemoryOrganizationsRepository/inMemoryOrganizationsRepository.mock';
+import { CreateOrganizationDTO } from './dtos/create-organization.dtos';
+import { OrganizationsRepository } from './organizations.repository';
 import { OrganizationsService } from './organizations.service';
 
 describe('OrganizationsService', () => {
   let service: OrganizationsService;
-
+  let inMemoryOrganizationsRepository: InMemoryOrganizationsRepository;
   beforeEach(async () => {
+    inMemoryOrganizationsRepository = new InMemoryOrganizationsRepository();
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrganizationsService],
+      providers: [
+        OrganizationsService,
+        { provide: OrganizationsRepository, useValue: inMemoryOrganizationsRepository },
+      ],
     }).compile();
 
     service = module.get<OrganizationsService>(OrganizationsService);
@@ -14,5 +22,32 @@ describe('OrganizationsService', () => {
 
   it('service should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('upsert > should update organization when it exists', async () => {
+    const spyUpdate = jest.spyOn(inMemoryOrganizationsRepository, 'update');
+
+    const organizationTest: CreateOrganizationDTO = inMemoryOrganizationsRepositoryMock[0];
+    organizationTest.email = 'NewEmail@gmail.com';
+    const result = await service.upsert(organizationTest);
+    const updatedPerson = inMemoryOrganizationsRepositoryMock.find((organization) => {
+      return organization.id_imobzi === organizationTest.id_imobzi;
+    });
+    expect(result).toEqual(updatedPerson);
+    expect(result.email).toBe('NewEmail@gmail.com');
+    expect(spyUpdate).toHaveBeenCalled();
+  });
+
+  it('upsert > should create organization when it NOT exists', async () => {
+    const spyCreate = jest.spyOn(inMemoryOrganizationsRepository, 'update');
+    const organizationTest: CreateOrganizationDTO = inMemoryOrganizationsRepositoryMock[0];
+    organizationTest.id = 9898;
+    const result = await service.upsert(organizationTest);
+    const updatedPerson = inMemoryOrganizationsRepositoryMock.find((organization) => {
+      return organization.id_imobzi === organizationTest.id_imobzi;
+    });
+    expect(result).toEqual(updatedPerson);
+    expect(result.id).toBe(9898);
+    expect(spyCreate).toHaveBeenCalled();
   });
 });
--- src/repository/organizations/organizations.service.ts
+++ src/repository/organizations/organizations.service.ts
@@ -1,5 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreateOrganizationDTO } from './dtos/create-organization.dtos';
+import { Organization } from './entities/organization.entity';
 import { OrganizationsRepository } from './organizations.repository';
 
 @Injectable()
-export class OrganizationsService implements Partial<OrganizationsRepository> {}
+export class OrganizationsService {
+  constructor(private readonly organizationsRepository: OrganizationsRepository) {}
+
+  async upsert(data: CreateOrganizationDTO): Promise<Organization> {
+    try {
+      const found = this.organizationsRepository.findById(data.id_imobzi);
+      if (found) {
+        return this.organizationsRepository.update(data.id_imobzi, data);
+      } else {
+        return this.organizationsRepository.create(data);
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
