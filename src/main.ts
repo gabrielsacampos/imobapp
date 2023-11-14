bb5ee8c4ca60aa31e4d7462924c65055aaa583b9
Gabriel <gabrielsa95.2@gmail.com>
2023-11-14 20:27:07 -0300

    vercel again

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as serverless from 'serverless-http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const logger = new Logger('bootstrap');
+  const expressApp = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+  await app.init();
 
-  app.useGlobalPipes(new ValidationPipe());
-
-  const port = 3000;
-  await app.listen(port);
-  logger.log(`application listening on port ${port}`);
+  return expressApp;
 }
+
 bootstrap();
+const handler = serverless(bootstrap);
+
+export { handler };
