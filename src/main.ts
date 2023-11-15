b32ce8bf3cc9b9fd3c76bc39ee816539a1e234f8
Gabriel <gabrielsa95.2@gmail.com>
2023-11-15 08:37:25 -0300

    setting sentry

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
